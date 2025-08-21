d697a1739f233bbd44d0c1e5a1b4e4cd0403d162
ONANA GREGOIRE LEGRAND <gregoirelegrandpsycho@gmail.com>
2025-08-21 08:59:19 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,7 @@
-// commands/ping.js
-const log = require('../logger')(module);
 module.exports = {
     name: 'ping',
-    description: 'Vérifie si le bot est en ligne.',
     adminOnly: false,
     run: async ({ sock, msg, replyWithTag }) => {
-        const remoteJid = msg.key.remoteJid;
-        log(`Commande reçue de ${remoteJid}`);
-        await replyWithTag(sock, remoteJid, msg, "🚀 Pong ! Je suis en ligne.");
-    }
-};  
+        await replyWithTag(sock, msg.key.remoteJid, msg, 'Pong! 🏓');
+    },
+};
