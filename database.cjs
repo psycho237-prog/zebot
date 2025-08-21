9c0dbf59a2d32578b788e72829193b83b523d35f
ONANA GREGOIRE LEGRAND <gregoirelegrandpsycho@gmail.com>
2025-08-21 09:16:03 +0100

    Update database.cjs

--- database.cjs
+++ database.cjs
@@ -1,12 +1,26 @@
-const users = {};
+const fs = require('fs');
+const path = require('path');
+const usersFile = path.join(__dirname, 'users.json');
+
+function loadUsers() {
+    if (!fs.existsSync(usersFile)) fs.writeFileSync(usersFile, '{}');
+    return JSON.parse(fs.readFileSync(usersFile));
+}
+
+function saveUsers(users) {
+    fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
+}
 
 module.exports = {
     getOrRegisterUser: async (id, name) => {
+        const users = loadUsers();
         if (!users[id]) users[id] = { name, commandsUsed: 0 };
+        saveUsers(users);
         return users[id];
     },
     incrementCommandCount: async (id) => {
-        if (!users[id]) users[id] = { name: 'Unknown', commandsUsed: 0 };
-        users[id].commandsUsed++;
-    },
+        const users = loadUsers();
+        if (users[id]) users[id].commandsUsed += 1;
+        saveUsers(users);
+    }
 };
