46fda8c6a671897b2af4e49a91660f863398a073
ONANA GREGOIRE LEGRAND <gregoirelegrandpsycho@gmail.com>
2025-08-21 09:31:07 +0100

    Update sticker.cjs

--- commands/sticker.cjs
+++ commands/sticker.cjs
@@ -1,8 +1,11 @@
+const logger = require('../logger.js')(module);
+
 module.exports = {
     name: "sticker",
     adminOnly: false,
     run: async ({ sock, msg, replyWithTag }) => {
         if (!msg.message.imageMessage) return replyWithTag(sock, msg.key.remoteJid, msg, "⚠️ Envoie une image !");
+        logger(`Sticker créé pour ${msg.key.remoteJid}`);
         await replyWithTag(sock, msg.key.remoteJid, msg, "✅ Sticker créé !");
     }
 };
